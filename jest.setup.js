f313d324db24c4979a561992ecfb453048c7e079
Man Vu <manvminh@gmail.com>
2025-06-03 22:03:29 -0400

    Fixed tests

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+if (!global.TextEncoder) global.TextEncoder = TextEncoder;
+if (!global.TextDecoder) global.TextDecoder = TextDecoder;
--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import Header from '@/components/Header/Header'
+import '@testing-library/jest-dom'
 
 test('renders header logo text', () => {
   render(
